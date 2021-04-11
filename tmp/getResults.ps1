256b9cf11bc6dbf76fb4e5d25c34a88b8ef1584a
alhaos <52729006+alhaos@users.noreply.github.com>
2021-04-11 21:20:53 +0500

    Create getResults.ps1

---
+++ tmp/getResults.ps1
@@ -0,0 +1,52 @@
+param(
+    [string]$accession = "1903120001"
+)
+
+
+$conn  = [System.Data.SqlClient.SqlConnection]::new('Server=LABHEALTHSERV2;Database=LincolnDiagnosticsLIS;User Id=dbro;Password=TecHP@$$123;MultipleActiveResultSets=true;')
+
+$commGetAccession = [System.Data.SqlClient.SqlCommand]::new()
+$commGetAccession.Connection = $conn
+$commGetAccession.CommandText = "select PatientID, PhysicianCode from tblAccessions where accession = @Accession"
+$commGetAccession.Parameters.Add([System.Data.SqlClient.SqlParameter]::new("@Accession", $accession)) | Out-Null
+
+
+$commGetPatient = [System.Data.SqlClient.SqlCommand]::new()
+$commGetPatient.Connection = $conn
+$commGetPatient.CommandText = "select * from tblPatient where patientid = @PatientID"
+
+"select * from tblResultedTests where accession =  @Accession"
+
+try{
+    $conn.Open()
+    $conn.State
+
+    $commGetAccessionDR = $commGetAccession.ExecuteReader()
+    if ($commGetAccessionDR.Read()){
+        $PatientID, $PhysicianCode = $commGetAccessionDR['PatientID', 'PhysicianCode']
+    }
+    else
+    {
+        throw "Accession not found"
+    }
+
+    $PatientID
+    $PhysicianCode
+
+    $commGetPatient.Parameters.Add([System.Data.SqlClient.SqlParameter]::new("@PatientID", $PatientID)) | Out-Null 
+
+    $commGetPatientDR = $commGetPatient.ExecuteReader()
+    while ($commGetPatientDR.Read()){
+        $commGetPatientDR['ptLastName']
+    } 
+
+    
+
+}
+
+catch{
+    $commGetPatientDR.Close()
+    $conn.Close()
+    Write-Error $_
+
+}
