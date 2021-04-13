e02462a9706987753a76a0b490c31c8151ff845e
alhaos <52729006+alhaos@users.noreply.github.com>
2021-04-13 22:00:59 +0500

    135

--- rdp/Untitled-1.ps1
+++ rdp/Untitled-1.ps1
@@ -1,1 +1,5 @@
-[System.Diagnostics.EventLog]::Exists('Windows PowerShell')
+
+
+[void][System.Reflection.Assembly]::LoadFrom("C:\Program Files (x86)\MySQL\MySQL Connector Net 8.0.23\Assemblies\v4.5.2\MySql.Data.dll" )
+$myconnection = [MySql.Data.MySqlClient.MySqlConnection]::new()
+$myconnection.ConnectionString = “server=yoursever;user id=youruser;password=yourpassword;database=yourdb;pooling=false”
